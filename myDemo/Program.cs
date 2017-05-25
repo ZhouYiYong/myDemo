2d6afb59110025a0bf5d7820952fad8d1b271846
ZhouYiYong <1048845636@qq.com>
2017-05-25 09:53:49 +0800

    演示

--- myDemo/Program.cs
+++ myDemo/Program.cs
@@ -17,6 +17,7 @@ namespace myDemo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
